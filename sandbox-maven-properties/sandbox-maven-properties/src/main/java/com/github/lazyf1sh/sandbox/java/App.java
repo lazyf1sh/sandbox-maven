a476c2104b31c64dc08b3da27bc825aaaffb861c
Ivan Kopylov <ivan.kopylov@t-systems.ru>
2019-02-13 12:54:30 +0300

    maven properties filling minimal example

---
+++ sandbox-maven-properties/sandbox-maven-properties/src/main/java/com/github/lazyf1sh/sandbox/java/App.java
@@ -0,0 +1,13 @@
+package com.github.lazyf1sh.sandbox.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
